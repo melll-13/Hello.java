5d04ec0508bea93734e9e11edc0a5df89087655a
melll-13 <122845141+melll-13@users.noreply.github.com>
2023-01-25 21:37:17 -0600

    Update cuadrado.java

--- cuadrado.java
+++ cuadrado.java
@@ -1,32 +1,24 @@
 public class Cuadrado {
-  public String ID; 
-  public int longitud;
-  public int diagonal; 
-  public int perimetro; 
-  public int area;
+ 
+ int lado;
+	
+	public Cuadrado(int lado) {
+    this.lado = lado;
+  }
 
-  public Cuadrado(String ID, int longitud, int perimetro, int area){
-    this.ID = ID;
-    this.longitud = longitud;
-    this.diagonal = diagonal;
-    this.perimetro = perimetro;
-    this.area = area;
+  public Cuadrado() {
+    this(25);
   }
-  public double calculaDiagonal(int longitud){
-    return longitud*Math.sqrt(2);
+
+  public double calcularDiagonal(int lado) {
+    return Math.sqrt(2*((double) lado*(double) lado));
   }
-  public float calculaPerimetro(int longitud){
-    return longitud*4;
+   public double calcularPerimetro(int lado) {
+    return lado*4;
   }
-  public float calculaArea(int longitud){
-    return longitud*longitud;
+  public double calcularArea(int lado) {
+    return lado*lado;
   }
-  }
-public class Hello {
-  public static void main(String[] args) {
-    Cuadrado Cinco = new Cuadrado ("Cinco", 5);
-      System.out.println(Cinco.calculaDiagonal(5));
-      System.out.println(Cinco.calculaPerimetro(5));
-      System.out.println(Cinco.calculaArea(5));
-  }
+
 }
+
